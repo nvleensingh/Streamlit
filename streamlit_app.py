c22cdb99ff62999320272af47b556a5712f91115
nvleensingh <77487249+nvleensingh@users.noreply.github.com>
2023-04-20 00:20:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ st.title("Largest of The Three Number")
 st.text("Enter the Three numbers and find the Largest amongest them")
 st.write(" ")
 
-num1 = st.number_input("Enter the Number 1: ", value = 0)
-num2 = st.number_input("Enter the Number 2: ", value = 0)
-num3 = st.number_input("Enter the Number 3: ", value = 0)
+# num1 = st.number_input("Enter the Number 1: ", value = 0)
+num1 = st.number_input("Enter the Number 1: ")
+num2 = st.number_input("Enter the Number 2: ")
+num3 = st.number_input("Enter the Number 3: ")
 
 
 def largest_calculator(a,b,c):
