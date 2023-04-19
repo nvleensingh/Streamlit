baf27c02ed36429dd94c4bb9f62454bbee45fab3
nvleensingh <77487249+nvleensingh@users.noreply.github.com>
2023-04-19 20:04:44 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+st.title("Largest of The Three Number")
+st.text("Enter the Three numbers and find the Largest amongest them")
+st.write(" ")
+
+num1 = st.number_input("Enter the Number 1: ", value = 0)
+num2 = st.number_input("Enter the Number Number 2: ", value = 0)
+num3 = st.number_input("Enter the Number Number 3: ", value = 0)
+
+
+def largest_calculator(a,b,c):
+    if(num1 == num2 == num3):
+        st.write("The numbers are Equal and The Number is: ",num1)
+    else:
+#         return("The Largest number is:", max(num1,num2,num3))
+        st.write("The Largest number is: ", max(num1,num2,num3))
+    
+
+if(st.button("Check")):
+    st.write(largest_calculator(num1,num2,num3))
+else:
+    st.write("Press The Button!!")
